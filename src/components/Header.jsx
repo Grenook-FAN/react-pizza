7bcb52337f14622b0c6006ba86ff2b2c2d1dd6e8
Grenook-FAN <artem.frizen02@gmail.com>
2020-11-12 19:04:06 +0600

    test

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -21,7 +21,7 @@ function Header() {
         <Link to='/cart'>
           <div className="header__cart">
             <Button className='button--cart'>
-              <span>{totalPrice}</span>
+              <span>{totalPrice} ₽</span>
               <div className="button__delimiter"></div>
               <svg width="18" height="18" viewBox="0 0 18 18" fill="none" xmlns="http://www.w3.org/2000/svg">
                 <path
--- src/redux/reducers/cart.js
+++ src/redux/reducers/cart.js
@@ -9,10 +9,12 @@ const cart = (state = initialState, action) => {
         case 'ADD_PIZZA_CART': {
             const newItems = {
                 ...state.items,
-                [action.payload.id]: !state.items[action.payload.id]
+                [action.payload.id]: {
+                    items: !state.items[action.payload.id]
                     ? [action.payload]
-                    : [...state.items[action.payload.id],
-                action.payload]
+                    : [...state.items[action.payload.id],action.payload]
+                    
+                }
             };
             const allPizzas = [].concat.apply([], Object.values(newItems))
             const totalPrice = allPizzas.reduce((sum, obj) => obj.price + sum,0)
