f7ec9b82e016e562113f6d0265d2eceec8039abd
Grenook-FAN <artem.frizen02@gmail.com>
2020-11-13 10:02:36 +0600

    Change cartItems

--- src/components/CartItem.jsx
+++ src/components/CartItem.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-function CartItem({name, size, type}) {
+function CartItem({name, size, type, price}) {
     return (
         <div className="cart__item">
             <div className="cart__item-img">
